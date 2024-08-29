cf72e84fc3208d9171bf42fcf61076f3102d6fb6
anzalkhan <anzalakhan@gmail.com>
2024-08-29 16:24:39 +0000

    LE1

---
+++ LE1/README.md
@@ -0,0 +1,14 @@
+# <p align="center">Lab 1: Environment Setup, Debugging, and C++ Refresh<p>
+
+## Introduction
+
+The objective of this programming assignment is to help you get ready for the remainings labs and programming assignments by covering some introductory refresher topics. Through this assignment, you will complete setting up your own development environment on GCP where you will work until the end of the semester. You will learn the use of debuggers (gdb) to debug your programs, and recap the core concepts of C/C++ programming.
+
+## Tasks
+
+You are given a simple program, **buggy.cpp**, that computes the area of shapes. Unfortunately it has several compilation errors and several runtime bugs that you are going to discover, debug, and fix with the help of three different debugging methods. Once you have corrected the program and committed to your Github repository, we will use the auto-grading feature of Gradescope to grade your project.
+
+Submit your program to Gradescope and it should autograde for you and generate a score.
+
+## Due Date
+The assignment is due midnight Sept 4. Note that the late penalty for submission is 1% per hour.
---
+++ LE1/buggy.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+struct Point {
+    int x, y;
+
+    Point () : x(), y() {}
+    Point (int _x, int _y) : x(_x), y(_y) {}
+};
+
+class Shape {
+    int vertices;
+    Point** points;
+public:
+    Shape (int _vertices) {
+        vertices = _vertices;
+        points = new Point*[vertices+1];
+        for (int i = 0; i < vertices+1; i++) {
+            points[i] = new Point();
+        }
+    }
+
+    ~Shape () {
+        for (int i = 0; i < vertices+1; i++) {
+            delete points[i];
+        }
+        delete[] points;
+    }
+
+    void addPoints (Point pts[]) {
+        for (int i = 0; i <= vertices; i++) {
+            memcpy(points[i], &pts[i%vertices], sizeof(Point));
+        }
+    }
+
+    double area () {
+        int temp = 0;
+        for (int i = 0; i < vertices; i++) {
+            // FIXME: there are two methods to access members of pointers
+            //        use one to fix lhs and the other to fix rhs
+            int lhs = points[i]->x * points[i+1]->y;
+            int rhs = (*points[i+1]).x * (*points[i]).y;
+            temp += (lhs - rhs);
+        }
+        double area = std::abs(temp)/2.0;
+        return area;
+    }
+};
+
+int main () {
+    // FIXME: create the following points using the three different methods
+    //        of defining structs:
+    //          tri1 = (0, 0)
+    //          tri2 = (1, 2)
+    //          tri3 = (2, 0)
+    Point tri1 = {0,0};
+    Point tri2(1,2);
+    Point tri3;
+    tri3.x = 2;
+    tri3.y = 0;
+    // adding points to tri
+    Point triPts[3] = {tri1, tri2, tri3};
+    Shape* tri = new Shape(3);
+    tri->addPoints(triPts);
+
+    // FIXME: create the following points using your preferred struct
+    //        definition:
+    //          quad1 = (0, 0)
+    //          quad2 = (0, 2)
+    //          quad3 = (2, 2)
+    //          quad4 = (2, 0)
+    Point quad1(0,0);
+    Point quad2(0,2);
+    Point quad3(2,2);
+    Point quad4(2,0);
+    // adding points to quad
+    Point quadPts[4] = {quad1, quad2, quad3, quad4};
+    Shape* quad = new Shape(4);
+    quad->addPoints(quadPts);
+
+    // FIXME: print out area of tri and area of quad
+	std::cout << tri->area() << std::endl;
+	std::cout << quad->area() << std::endl;
+    
+    delete tri;
+    delete quad;
+}
